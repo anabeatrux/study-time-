7980061132508074c5831d64209af0524f52e235
anabeatrux <biamatos958@gmail.com>
2025-06-19 07:57:31 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('button').addEventListener('click', () => {
+  const resposta = document.getElementById('resposta');
+  resposta.innerHTML = "<p>🤖 Em breve nossa IA responderá aqui! (Integração futura com IA)</p>";
+});
